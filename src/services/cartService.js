45e25b0db156a36ddf411ba0c402d02e9b0abda9
Sarankumar09-cse <sarankumar.yarabarla@gmail.com>
2025-09-25 12:44:07 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8081/api/cart";
+const API_URL = "http://ec2-54-226-215-191.compute-1.amazonaws.com:8081/api/cart";
 
 const API_BASE = "http://localhost:8081/api/cart";
 
